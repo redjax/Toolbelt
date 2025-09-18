0d5b7d7a10d1c9be7ff7310895e68f5e0e6d9433
redjax <5534031+redjax@users.noreply.github.com>
2025-09-17 22:12:06 -0400

    fix(consts): Add constsants for readme file & markers

--- .repo/apps/toolbelt/src/toolbelt/constants.py
+++ .repo/apps/toolbelt/src/toolbelt/constants.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 
 
-__all__ = ["REPO_ROOT", "TOOLS_JSON"]
+__all__ = [
+    "REPO_ROOT",
+    "TOOLS_JSON",
+    "README_FILE",
+    "MARKER_START_STR",
+    "MARKER_END_STR",
+]
 
 ## Absolute path to this script's directory
 THIS_DIR = Path(__file__).resolve().parent
@@ -10,3 +16,8 @@ THIS_DIR = Path(__file__).resolve().parent
 REPO_ROOT = THIS_DIR.parent.parent.parent.parent.parent
 ## Path to tools.json
 TOOLS_JSON = REPO_ROOT / ".repo" / "tools.json"
+## Path to main README.md
+README_FILE = REPO_ROOT / "README.md"
+
+MARKER_START_STR = "<!-- MARKER-TOOLS:START -->"
+MARKER_END_STR = "<!-- MARKER-TOOLS:END -->"
